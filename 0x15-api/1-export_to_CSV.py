6f56ea9ed905130fe07bd3e91d4bb85e4fd4ef9d
EsayasA <esayasaregawi29@gmail.com>
2024-02-19 10:53:37 -0800

    task0

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""
+Python script that, using a REST API, for a given employee ID,
+"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    resp = get('https://jsonplaceholder.typicode.com/todos/')
+    info = resp.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response = get('https://jsonplaceholder.typicode.com/users')
+    data = response.json()
+
+    for k in data:
+        if k.get('id') == int(argv[1]):
+            emp = k.get('name'#!/usr/bin/python3
+
+"""
+Python script that 
+"""
+
+from requests import get
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    resp = get('https://jsonplaceholder.typicode.com/todos/')
+    info = resp.json()
+
+    rt = []
+    response = get('https://jsonplaceholder.typicode.com/users')
+    data = response.json()
+
+    for k in data:
+        if k['id'] == int(argv[1]):
+            emp = i['username']
+
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for k in info:
+
+            rt = []
+            if k['userId'] == int(argv[1]):
+                rt.append(i['userId'])
+                rt.append(emp)
+                rt.append(k['completed'])
+                rt.append(k['title'])
+
+                wr.writerow(rt)
+
+    for k in info:
+        if k.get('userId') == int(argv[1]):
+            total += 1
+
+            if k.get('completed') is True:
+                completed += 1
+                tasks.append(k.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(emp, completed,
+                                                          total))
+
+    for i in tasks:
+        print("\t {}".format(i))
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Python script that exports
+"""
+
+from requests import get
+import json
+
+if __name__ == "__main__":
+    ans = get('https://jsonplaceholder.typicode.com/todos/')
+    da = ans.json()
+
+    roww = []
+    ans2 = get('https://jsonplaceholder.typicode.com/users')
+    da2 = ans2.json()
+
+    new = {}
+
+    for k in da2:
+
+        roww = []
+        for j in da:
+
+            new2 = {}
+
+            if k['id'] == j['userId']:
+
+                new2['username'] = k['username']
+                new2['task'] = j['title']
+                new2['completed'] = j['completed']
+                roww.append(new2)
+
+        new[k['id']] = roww
+
+    with open("todo_all_employees.json",  "w") as x:
+
+        json_obj = json.dumps(new)
+        x.write(json_obj)
